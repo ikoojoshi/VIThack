9a6aab9cf6e5153f05b3f2bae33734933fd78341
Vatsal Kandoi <vatsalkandoi1998@gmail.com>
2019-09-22 17:14:13 +0530

    dd

--- a.py
+++ a.py
@@ -5,8 +5,10 @@
 from pymongo import MongoClient
 from HPchatbot import readContents, words, find
 
-client = MongoClient('mongodb://localhost:27017/myDatabase')
+client = MongoClient('mongodb://admin123:admin123@ds353007.mlab.com:53007/mydatabase')
 questions, answers = readContents();
+
+user = client.db.user.insert_one({"queries":[]})
 
 for i in range(len(questions)):
     aa = client.db.dislike.insert_one({"question":questions[i],"answer":answers[i],"dislikes":0})
